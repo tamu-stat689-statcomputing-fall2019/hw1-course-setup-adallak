1aabff3cd8a117e66a03a6eef9f6ec294e07b675
Aramayis Dallakyan <armopost@yahoo.com>
2019-08-29 17:13:11 -0500

    Used functions generateY, beta_LS and calculateMSE

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -1,5 +1,6 @@
 # Generate data from linear regression model and calculate the least squares vector of coefficients
 #####################################################################################################
+source("FunctionsLM.R") ## Calling the file which stores the functions
 
 # Model parameters
 n = 100 # sample size
@@ -9,7 +10,20 @@ beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
 # [ToDo] Use generateY function to generate Y
-
+y = generateY(X,beta,sigma)
 # [ToDo] Use calculateBeta function to calculate beta_LS
-
+beta_LS= calculateBeta(X,y)
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+calculateMSE(beta,beta_LS)
+
+### Checking that linear regression function works properly
+m1<-lm(y~0+X)
+beta_lm = coef(m1)
+
+eps = 10e-10 ## Threshold two compare two results
+if(as.numeric(crossprod(beta_lm-beta_LS))<eps)
+{
+  print("identical")
+}else{
+  print("Non-identical")
+}
